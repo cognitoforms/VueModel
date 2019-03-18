8668bdc441a2481c39951067d0104c872b309806
Bryan Matthews <mattheyan@gmail.com>
2019-03-18 16:21:23 -0400

    ﻿[c6b995e7b9f35c6d82f9a5bb747e36383a66b666]: Fix imports

--- lib/model.js/src/entity-serializer.ts
+++ lib/model.js/src/entity-serializer.ts
@@ -1,4 +1,5 @@
-import { Entity, isEntityType, Type } from ".";
+import { isEntityType, Type } from "./type";
+import { Entity } from "./entity";
 import { Property } from "./property";
 
 export interface PropertySerializationResult {
@@ -105,8 +106,11 @@ export class EntitySerializer {
 
 	private static defaultPropertyConverter(prop: Property, value: any): PropertySerializationResult {
 		let result = { key: prop.name, value };
-		if (value && isEntityType(prop.propertyType)) {
-			result.value = value.serialize();
+		if (value) {
+			if (prop.isList)
+				result.value = value.slice();
+			if (isEntityType(prop.propertyType))
+				result.value = value.serialize();
 		}
 		return result;
 	}
