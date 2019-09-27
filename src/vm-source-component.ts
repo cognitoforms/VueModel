a8be3614975b74015f0b697de94a9ee1ca03bdf9
Bryan Matthews <bryan@cognitoforms.com>
2019-09-27 15:31:02 -0400

    Change vm-source component to use source-path-mixin
    The components were virtually the same with the exception of the render method

--- src/vm-source-component.ts
+++ src/vm-source-component.ts
@@ -1,54 +1,10 @@
-import Vue, { VNode } from "vue";
-import { Component, Prop, Watch } from "vue-property-decorator";
-import { SourceAdapter, isSourceAdapter, applyOverridesToSourceAdapter, hasOverrideValue } from "./source-adapter";
-import { SourcePathAdapter } from "./source-path-adapter";
-import { Entity } from "@cognitoforms/model.js";
+import { VNode } from "vue";
+import { Component } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
+import { SourcePathMixin } from "./source-path-mixin";
 
 @Component
-export class VMSource extends Vue {
-	@Prop({ type: [String, Object] })
-	source: string | SourceAdapter<any>;
-
-	@Prop(String)
-	label: string;
-
-	@Watch("label")
-	onLabelChanged(label: string): void {
-		this.onOverrideValueChanged(label, String);
-	}
-
-	@Prop(String)
-	helptext: string;
-
-	@Watch("helptext")
-	onHelptextChanged(helptext: string): void {
-		this.onOverrideValueChanged(helptext, String);
-	}
-
-	@Prop({ type: Boolean, default: null })
-	readonly: boolean;
-
-	@Watch("readonly")
-	onReadonlyChanged(readonly: boolean): void {
-		this.onOverrideValueChanged(readonly, Boolean);
-	}
-
-	@Prop({ type: Boolean, default: null })
-	required: boolean;
-
-	@Watch("required")
-	onRequiredChanged(required: boolean): void {
-		this.onOverrideValueChanged(required, Boolean);
-	}
-
-	get $source(): SourceAdapter<any> {
-		if (isSourceAdapter(this.source)) {
-			return applyOverridesToSourceAdapter(this.source, this);
-		}
-
-		return new SourcePathAdapter<Entity, any>({ parent: this, propsData: { source: this.source, overrides: this } });
-	}
-
+export class VMSource extends mixins(SourcePathMixin) {
 	render(): VNode {
 		var slotNodes = this.$scopedSlots.default({
 			$source: this.$source
@@ -59,11 +15,5 @@ export class VMSource extends Vue {
 		}
 
 		return slotNodes[0];
-	}
-
-	onOverrideValueChanged(value: string | boolean, type: StringConstructor | BooleanConstructor): void {
-		if (isSourceAdapter(this.source) && hasOverrideValue(value, type)) {
-			throw new Error("Cannot apply overrides to source of type '" + this.source.constructor.name + "'.");
-		}
 	}
 }
