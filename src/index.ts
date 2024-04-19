584777fe29dab6200d0d7a29420b2ec44e812e00
Bryan Matthews <bryan@cognitoforms.com>
2024-04-19 11:59:49 -0400

    Remove model.js re-exports

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,6 @@ import "@cognitoforms/model.js/lib/resource-en";
 
 export default VueModel;
 
-// Export any additional *types* to be referenced externally
-export type { Model, ModelOptions, ModelLocalizationOptions } from "@cognitoforms/model.js";
-export type { TypeOptions, TypeExtensionOptions, RuleOrMethodFunctionOrOptions } from "@cognitoforms/model.js";
-export type { Entity, EntityOfType, EntityConstructorForType, TypeOfType } from "@cognitoforms/model.js";
-export type { CultureInfo } from "@cognitoforms/model.js";
-export type { Property, PropertyOptions, PropertyBooleanFunction, PropertyBooleanFunctionAndOptions } from "@cognitoforms/model.js";
-export type { PropertyInjector, PropertySerializationResult } from "@cognitoforms/model.js";
-export { PropertyConverter, IgnoreProperty } from "@cognitoforms/model.js";
-export { isEntityType, isValueType } from "@cognitoforms/model.js";
 export { isSourceAdapter } from "./source-adapter";
 export { preventVueObservability } from "./vue-model-observability";
 export type { SourceRootAdapter } from "./source-root-adapter";
